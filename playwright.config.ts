17ef4f57bce3ec7bdb08b6a354fc937703a0b168
deepfates <deepfates@gmail.com>
2026-08-28 13:40:16 -0700

    Support production acceptance testing

--- playwright.config.ts
+++ playwright.config.ts
@@ -8,11 +8,11 @@ export default defineConfig({
   retries: process.env.CI ? 1 : 0,
   reporter: process.env.CI ? 'github' : 'line',
   use: {
-    baseURL: 'http://127.0.0.1:5173',
+    baseURL: process.env.PLAYWRIGHT_BASE_URL || 'http://127.0.0.1:5173',
     trace: 'retain-on-failure',
   },
   projects: [{name: 'chromium', use: {...devices['Desktop Chrome']}}],
-  webServer: {
+  webServer: process.env.PLAYWRIGHT_BASE_URL ? undefined : {
     command: 'npm run dev',
     url: 'http://127.0.0.1:5173',
     reuseExistingServer: !process.env.CI,
